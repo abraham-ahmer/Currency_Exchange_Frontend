67382be3d8f6de86ee3e86e047e51216c7220365
Abraham Ahmer <utdahmer@gmail.com>
2026-04-26 10:41:54 +0500

    added automatic refresh token function in login page.

--- login.js
+++ login.js
@@ -1,7 +1,50 @@
 const loginForm = document.querySelector("#loginForm");
 const loader = document.querySelector("#Loader"); // loader element by ID
 
-loginForm.addEventListener("submit", async (e) => {
+// get new access token using refresh token
+async function refreshAccessToken() {
+  const refreshToken = localStorage.getItem("refresh_token"); // get ref tokens from localStorage.
+  if (!refreshToken) return null; // if no ref token, return nothin
+
+  try {
+    const response = await fetch("https://currency-exchange-backend-zi4p.onrender.com/login/refresh",{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ refresh_token: refreshToken }),
+      },
+    );
+
+    if (response.ok) {
+      const data = await response.json(); // If backend accepts the refresh token, it issues new access token.
+      localStorage.setItem("access_token", data.access_token); // we overwrite that expired access token
+      return data.access_token;
+    } else {
+      // if refresh failed. 7 days have passed... backend checked it... FORCED LOGOUT
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      window.location.href =
+        "https://abraham-ahmer.github.io/Currency_Exchange_Frontend/index.html";
+      return null;
+    }
+  } catch (error) {
+    return null;
+  }
+}
+
+
+
+// runs second and calls refreshAccessToken
+function startRefreshTimer() {
+  //25 minutes in ms
+  const interval = 25 * 60 * 1000;
+  setInterval(async () => {
+    await refreshAccessToken();
+  }, interval);
+}
+
+
+// runs first and call startRefreshTimer 
+loginForm.addEventListener("submit", async (e) => {  
   e.preventDefault();
 
   loader.style.display = "block"; // show loader immediately on submit
@@ -26,11 +69,18 @@ loginForm.addEventListener("submit", async (e) => {
     if (response.ok) {
       const data = await response.json();
       alert(`Login successful. Welcome back`);
+
+      // saving both access and refreshing tokens
       localStorage.setItem("access_token", data.access_token);
+      localStorage.setItem("refresh_token", data.refresh_token);
+
       loginForm.reset();
       window.location.href =
         "https://abraham-ahmer.github.io/Currency_Exchange_Frontend/index.html";
       updateNavbar();
+
+      // start refresh timer 
+      startRefreshTimer();
     } else {
       const error = await response.json();
       alert(`Error occurred while logging in: ${error.detail}`);
@@ -41,3 +91,31 @@ loginForm.addEventListener("submit", async (e) => {
     alert(`Network error: ${err.message}`);
   }
 });
+
+
+
+
+
+
+// for other endpoints in frontend calling backend endpoints
+async function fetchWithAuth(url, options = {}) {
+  let token = localStorage.getItem("access_token");
+
+  let response = await fetch(url, {
+    ...options, /// these three dots spreads everything inside it as obj
+    headers: { ...options.headers, Authorization: `Bearer ${token}` },
+  }); 
+
+  // if unauthorized, try refresh.
+  if (response.status === 401) {
+    let token = await refreshAccessToken();
+    if (token) {
+      let response = await fetch(url, {
+        ...options,
+        headers: { ...options.headers, Authorization: `Bearer ${token}` },
+      });
+    }
+  }
+  return response;
+}
+
